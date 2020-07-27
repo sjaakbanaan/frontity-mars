dad6bd00728bbecc5c34b8e0a44e2b60790ca4ee
David Tuk <davidtuk@gmail.com>
2020-07-27 21:16:15 +0200

    initial commit with custom additions

---
+++ frontity.settings.js
@@ -0,0 +1,58 @@
+const settings = {
+  "name": "frontity-mars",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://ubierfestival.nl/wp-hike-test/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+    "@frontity/head-tags"
+  ]
+};
+
+export default settings;
